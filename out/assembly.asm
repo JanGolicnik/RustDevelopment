ea069ba61bd1da80141300334ee8a4726f345bb0
Jan <jan@nejka.net>
2023-10-25 20:15:18 +0200

    comparison operator example

--- out/assembly.asm
+++ out/assembly.asm
@@ -2,19 +2,58 @@ global _start:
 _start:
     push rbp
     mov rbp, rsp
-    mov rdi, 2
+    mov rdi, 1
+    push rdi
+    mov rdi, [rbp - 8]
+    push rdi
+    mov rdi, 1
+    pop rax
+    cmp rax, rdi
+    je LABEL2
+LABEL3:
+    mov rdi, 0
+    jmp LABEL4
+LABEL2:
+    mov rdi, 1
+LABEL4:
+    cmp rdi, 0
+    je LABEL1
+    mov rdi, 1
     push rdi
     mov rdi, 2
     pop rax
-    cmp rax, rdi
-    je LABEL1
-LABEL2:
-    mov rdi, 0
-    jmp LABEL3
-LABEL1:
-    mov rdi, 1
-LABEL3:
+    add rax, rdi 
+    mov rdi, rax
     push rdi
+    mov rdi, 3
+    pop rax
+    add rax, rdi 
+    mov rdi, rax
+    push rdi
+    mov rdi, 4
+    pop rax
+    add rax, rdi 
+    mov rdi, rax
+    push rdi
+    mov rdi, [rbp - 16]
+    push rdi
+    mov rdi, [rbp - 8]
+    pop rax
+    cmp rax, rdi
+    jg LABEL6
+LABEL7:
+    mov rdi, 0
+    jmp LABEL8
+LABEL6:
+    mov rdi, 1
+LABEL8:
+    cmp rdi, 0
+    je LABEL5
+    mov rdi, [rbp - 16]
+    mov rax, 60
+    syscall
+LABEL5:
+LABEL1:
     mov rdi, [rbp - 8]
     mov rax, 60
     syscall
